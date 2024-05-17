8b59441721ed6e985c81aa5a3a4f7c7c58260c43
Kirill Lukyanov <kirs2000@mail.ru>
2024-05-17 13:52:55 +0300

    numbers tasks added

--- src/ru/lukyanov/collections/countwordsinstringwithmap/App.java
+++ src/ru/lukyanov/collections/countwordsinstringwithmap/App.java
@@ -1,9 +1,11 @@
 package ru.lukyanov.collections.countwordsinstringwithmap;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class App {
+
 
     public static void countWords(String str) {
         String[] arr = str.split(" ");
@@ -20,8 +22,26 @@ public class App {
         System.out.println(map);
     }
 
+    public static void iterMap() {
+        Map<Integer, String> integerStringMap = new HashMap<>();
+        integerStringMap.put(1,"str1");
+        integerStringMap.put(2,"str2");
+        integerStringMap.put(3,"str3");
+
+        for (Map.Entry<Integer, String> integerStringEntry : integerStringMap.entrySet()) {
+            System.out.println(integerStringEntry.getKey() + " " + integerStringEntry.getValue());
+        }
+
+        Iterator iterator = integerStringMap.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry <Integer, String> count = (Map.Entry) iterator.next();
+            System.out.println(count.getKey() + " " + count.getValue());
+        }
+    }
+
     public static void main(String[] args) {
 String str = "Напишите программу на Java для подсчета количества конкретных слов в строке, используя HashMap. Напишите программу на Java";
     countWords(str);
+    iterMap();
     }
 }
---
+++ src/ru/lukyanov/numbers/isaprime/App.java
@@ -0,0 +1,25 @@
+package ru.lukyanov.numbers.isaprime;
+
+public class App {
+
+    public static void isAPrime(int number) {
+        int tempNumber;
+        boolean numberIsPrime = true;
+
+        for (int x = 2; x <= number / 2; x++) {
+            tempNumber = number % x;
+            if (tempNumber == 0) {
+                numberIsPrime = false;
+                break;
+            }
+        }
+        if (numberIsPrime)
+            System.out.println(number + " - число является простым");
+        else
+            System.out.println(number + " - число не является простым");
+    }
+
+    public static void main(String[] args) {
+        isAPrime(4);
+    }
+}
