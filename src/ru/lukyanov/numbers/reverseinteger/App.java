495c3b82e2a06961458854a48f7a993cf39b42ca
Kirill Lukyanov <kirs2000@mail.ru>
2024-07-01 14:48:51 +0300

    reverseinteger task added

--- src/ru/lukyanov/numbers/reverseinteger/App.java
+++ src/ru/lukyanov/numbers/reverseinteger/App.java
@@ -3,10 +3,11 @@ package ru.lukyanov.numbers.reverseinteger;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println(revInteger(-12345L));
+        System.out.println(revIntegerV1(-12345L));
+        System.out.println(revIntegerV2(-1234567));
     }
 
-    public static Integer revInteger(Long number) {
+    public static Integer revIntegerV1(Long number) {
         long num = number;
         if (num < Integer.MIN_VALUE || num > Integer.MAX_VALUE) return 0;
         StringBuilder stringBuilder;
@@ -20,6 +21,24 @@ public class App {
             stringBuilder.reverse();
         }
         return Integer.valueOf(stringBuilder.toString());
+    }
+
+    private static Integer revIntegerV2(int num) {
+        int res = 0;
+        while (num != 0) {
+            int module = num % 10;
+            if ((res > Integer.MAX_VALUE / 10) || (res == Integer.MAX_VALUE / 10 && module > 7)) {
+                return 0;
+            }
+            if ((res < Integer.MIN_VALUE / 10) || (res == Integer.MIN_VALUE / 10 && module < -8)) {
+                return 0;
+            }
+            res *= 10;
+            res += module;
+            num = num / 10;
+
+        }
+        return res;
     }
 
 }
